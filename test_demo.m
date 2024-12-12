b5c2a0227b62f58279c84e02a1835e59b0ca4621
卿欣 <junqing.huang@ugent.be>
2024-12-12 15:45:02 +0100

    Add files via upload

---
+++ test_demo.m
@@ -0,0 +1,19 @@
+clc
+clear
+% close all
+addpath ./code
+
+u_i = im2double(imread('./data/boy.jpg')); % input image
+
+lambda              = 0.005; % boy.jpg
+alpha               = 0.006;
+beta                = 0.001;
+
+% lambda              = 0.004; % zeugma.png
+% alpha               = 0.007;
+% beta                = 0.002;
+
+% maximum number of iterations, and it may take minutes for a large number iterations. 
+iters               = 1500;      
+u                   = admm_solver(u_i, lambda, alpha, beta, iters);
+figure, imshow([u_i u])
